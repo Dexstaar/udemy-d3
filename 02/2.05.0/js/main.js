fb436a99378806779824516404535b0543a2aa76
dexstar <dexstaar@gmail.com>
2018-05-23 20:47:58 +0800

    lec 11

--- 02/2.05.0/js/main.js
+++ 02/2.05.0/js/main.js
@@ -3,3 +3,13 @@
 *    Mastering Data Visualization with D3.js
 *    2.5 - Activity: Adding SVGs to the screen
 */
+
+var svg = d3.select("#chart-area").append("svg")
+    .attr("width", 500)
+    .attr("height", 400);
+
+var circle = svg.append("circle")
+    .attr("cx", 100)
+    .attr("cy", 250)
+    .attr("r", 70)
+    .attr("fill", "grey");
